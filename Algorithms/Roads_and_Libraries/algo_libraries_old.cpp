4106fd4234e037d22cda946ce7723adfdc2f9eab
Bradley Evans <bevan006@ucr.edu>
2017-06-28 07:28:57 +0000

    HackerRank accepted solution as given in algo_libraries.cpp.

--- Algorithms/Roads_and_Libraries/algo_libraries_old.cpp
+++ Algorithms/Roads_and_Libraries/algo_libraries_old.cpp
@@ -36,7 +36,7 @@ class Graph {
 private:
 	int V;							// number of veritices
 	struct AdjList* array;
-	long long cost;
+	double cost;
 public:
 	// Constructor for a new graph
 	Graph(int V) {
@@ -65,17 +65,79 @@ public:
 		newNode->next = array[dest].head;
 		array[dest].head = newNode;
 	}
-	void libraryDFS_recursion(int n, bool visited[], long long libCost, long long roadCost) {
+	// Show adjacency list.
+	void printGraph() {
+		for (int v=0; v<V; ++v) {
+			AdjListNode* current = array[v].head;
+			cout << "\n Adjacency list of vertex " << v << "\n (head)";
+			while(current != NULL) {
+				cout  << "-> " << current->dest;
+				current = current->next;
+			}
+		}
+		cout << endl;
+	}
+	// Breadth First Search
+	int libraryBFS(int libCost, int roadCost) {
+	    // cout << "Beginning BFS." << endl;
+	    bool visited[V+1];
+	    for(int i=0; i<V; i++) {
+	        visited[i] = false;
+	    }
+	    
+        int n = 1;
+        AdjListNode* neighbor = array[n].head;
+	    list<int> queue;
+	    queue.push_back(n);
+    	// cout << "Build a library at " << n << endl;
+    	cost = cost + libCost;
+		// cout << "Current cost: " << cost << endl;
+	    while(!queue.empty()) {
+		    while(!queue.empty()) {
+		        n = queue.front();
+		        queue.pop_front();
+		        // cout << n << "->";
+		        visited[n] = true;
+		        neighbor = array[n].head;
+		        while(neighbor != NULL) {
+		        	if (!visited[neighbor->dest]) {
+		        		queue.push_back(neighbor->dest);
+		        		visited[neighbor->dest] = true;
+		        		if (libCost < roadCost) {
+		        			// cout << "Build a library at " << neighbor->dest << endl;
+		        			cost = cost+libCost;
+		        		} else {
+		        			// cout << "Build a road from " << n << " to " << neighbor->dest << endl;
+		        			cost = cost+roadCost;
+		        		}
+		        	}
+		        	neighbor = neighbor->next;
+		        }
+		    }
+		    // cout << "END. BFS Complete for this portion of the graph." << endl
+		    // 	 << "Checking for other unvisited nodes..." << endl;
+		    for (int i=1; i<V+1; i++) {
+		    	if (!visited[i]) {
+		    		queue.push_back(i);
+		    		// cout << i << " was unvisited!" << endl;
+			    	// cout << "Build a library at " << i << endl;
+			    	cost = cost + libCost;
+		    		break;
+		    	}
+		    }
+	    }
+	    // cout << "No other unvisited nodes found. Traversal complete." << endl;
+	    // cout << "***** Anticipated cost: " << cost << endl;
+	    return cost;
+	}
+	void libraryDFS_recursion(int n, bool visited[], int libCost, int roadCost) {
 		AdjListNode* neighbor = array[n].head;
-		// cout << n << "->";
+		//cout << n << "->";
 		visited[n] = true;
 		if (roadCost < libCost) {
 			this->cost = this->cost + roadCost;
 		} else {
 			this->cost = this->cost + libCost;
-		}
-		if (this->cost < 0) {
-			cout << "Uh oh. Cost = " << this->cost << endl;
 		}
 		while (neighbor != NULL) {
 			if (!visited[neighbor->dest]) {
@@ -84,24 +146,20 @@ public:
 			neighbor = neighbor->next;
 		}
 	}
-	long long libraryDFS(long long libCost, long long roadCost) {
+	int libraryDFS(int libCost, int roadCost) {
 		bool visited[V+1];
 		bool complete = false;
 		for (int i=0; i<V+1; i++) {
 			visited[i] = false;
 		}
 		this->cost = 0;
-		// cout << "Performing DFS traversal." << endl;
+		//cout << "Performing DFS traversal." << endl;
 		int n = 1;
-		int iter = 1;
 		AdjListNode* neighbor;
 		while (!complete) {
 			neighbor = array[n].head;
 			this->cost = this->cost + libCost;
-			if (this->cost < 0) {
-				cout << "Uh oh. Cost = " << this->cost << endl;
-			}
-			// cout << n << "->";
+			//cout << n << "->";
 			visited[n] = true;
 			while (neighbor != NULL) {
 				if (!visited[neighbor->dest]) {
@@ -109,17 +167,16 @@ public:
 				}
 				neighbor = neighbor->next;
 			}
-			// cout << "COMPLETE. Reached end of graph starting from " << n << endl;
-			for (int i=iter;i<V+1;i++) {
+			//cout << "COMPLETE. Reached end of graph starting from " << n << endl;
+			for (int i=1; i<V+1; i++) {
 				if (!visited[i]) {
 					//cout << i << " was unvisited!" << endl;
 					n = i;
 					break;
 				} else if (i == V) {
-					// cout << "No more unvisited nodes detected." << endl;
+					//cout << "No more unvisited nodes detected." << endl;
 					complete = true;
 				}
-				iter = i;
 			}
 		}
 		return this->cost;
@@ -135,8 +192,8 @@ int main() {
     for(int a0 = 0; a0 < q; a0++){
         int n;      // number of cities
         int m;      // number of roads
-        long long x;
-        long long y;
+        long x;
+        long y;
         cin >> n >> m >> x >> y;
 
         // Create a graph with n verticies (representing cities).
@@ -148,8 +205,18 @@ int main() {
             cin >> city_1 >> city_2;
             graph.addEdge(city_1,city_2);
         }
+        // input verification, problem set
+        // cout << "Query " << a0 + 1 << endl;
+        // cout << "Number of cities: " << n << endl;
+        // cout << "Number of bidirectional roads: " << m << endl;
+        // cout << "Cost of building a library: " << x << endl;
+        // cout << "Cost of building a road: " << y << endl;
+
+        // cout << "Adjacency list for query " << a0 << ":" << endl;
+        // graph.printGraph();
+        // cout << graph.libraryBFS(x,y) << endl;
         cout << graph.libraryDFS(x,y) << endl;
         
     }
     return 0;
-}
+}	
